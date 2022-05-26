3ed4f12b848bfc6d853fc40abf4a1e2c1d9d3c0b
Luis Tellez <luis@luistellez.com>
2022-05-25 20:41:59 -0700

    feautre: add .nojekyll file

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,3 +1,50 @@
+let editButton = document.querySelector(".profile__edit-button");
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".modal__overlay");
+let formName = document.querySelector(".modal__profile-name");
+let formTitle = document.querySelector(".modal__profile-title");
+let profileName = document.querySelector(".profile__name");
+let profileTitle = document.querySelector(".profile__title");
+
+editButton.addEventListener("click", modalOpen);
+
+function modalOpen() {
+  overlay.classList.add("modal__overlay_active");
+  modal.classList.add("modal_active");
+  formName.value = profileName;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Busquemos el formulario en el DOM
 let formElement = document.querySelector(.modal__form) // Utiliza el método querySelector()
 
