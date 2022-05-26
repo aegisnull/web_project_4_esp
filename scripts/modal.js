613a9aee90fe87e6a15030f7eb7c219e37fb94b8
Luis Tellez <luis@luistellez.com>
2022-05-26 14:32:17 -0700

    fix: remove overlay from javascript

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,6 +1,5 @@
 let editButton = document.querySelector(".profile__edit-button");
 let modal = document.querySelector(".modal");
-let overlay = document.querySelector(".modal__overlay");
 let formName = document.querySelector(".modal__profile-name");
 let formTitle = document.querySelector(".modal__profile-title");
 let profileName = document.querySelector(".profile__name");
@@ -9,7 +8,6 @@ let profileTitle = document.querySelector(".profile__title");
 editButton.addEventListener("click", modalOpen);
 
 function modalOpen() {
-  overlay.classList.add("modal__overlay_active");
   modal.classList.add("modal_active");
   formName.value = profileName.textContent;
   formTitle.value = profileTitle.textContent;
