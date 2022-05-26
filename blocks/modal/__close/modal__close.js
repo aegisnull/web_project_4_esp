d1ec5be21e914b6f983ca9fd5109be96f4054a4c
Luis Tellez <luis@luistellez.com>
2022-05-25 17:39:53 -0700

    refactor: simplified modal structure

--- blocks/modal/__close/modal__close.js
+++ blocks/modal/__close/modal__close.js
@@ -6,7 +6,5 @@ let overlay = document.querySelector(".modal__overlay");
 editButton.addEventListener("click", modalOpen);
 
 function modalOpen() {
-  modal.classList.add("modal_active");
-  container.classList.add("modal__container_active");
   overlay.classList.add("modal__overlay_active");
 }
