064f5582f0245c798c282bf96506e29d9b2cc04f
Luis Tellez <luis@luistellez.com>
2022-05-26 14:52:55 -0700

    feature: add code to close modal on button click

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,4 +1,3 @@
-//Controls for edit modal opening
 // Variable initialization
 let editButton = document.querySelector(".profile__edit-button");
 let modal = document.querySelector(".modal");
@@ -6,7 +5,9 @@ let formName = document.querySelector(".modal__profile-name");
 let formTitle = document.querySelector(".modal__profile-title");
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__title");
+let closeButton = document.querySelector(".modal__close");
 
+//Controls for edit modal opening
 // Event listener for edit button click
 editButton.addEventListener("click", modalOpen);
 
@@ -15,4 +16,12 @@ function modalOpen() {
   modal.classList.add("modal_active");
   formName.value = profileName.textContent;
   formTitle.value = profileTitle.textContent;
+}
+
+//Controls for edit modal closing
+// Event listener for close button click
+closeButton.addEventListener("click", modalClose);
+
+function modalClose() {
+  modal.classList.remove("modal_active");
 }
