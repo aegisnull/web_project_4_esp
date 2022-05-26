650f4a21c9e5ad0fe20b9e6bb38a7fe383075205
Luis Tellez <luis@luistellez.com>
2022-05-26 14:34:34 -0700

    style: add comments to javascript code

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,3 +1,5 @@
+//Controls for edit modal opening
+// Variable initialization
 let editButton = document.querySelector(".profile__edit-button");
 let modal = document.querySelector(".modal");
 let formName = document.querySelector(".modal__profile-name");
@@ -5,8 +7,10 @@ let formTitle = document.querySelector(".modal__profile-title");
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__title");
 
+// Event listener for edit button click
 editButton.addEventListener("click", modalOpen);
 
+// Function to open modal and in the process set the values of the form fields same as the profile fields
 function modalOpen() {
   modal.classList.add("modal_active");
   formName.value = profileName.textContent;
