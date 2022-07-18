2cc40e5236e0cba8534012d2c93fd71f7f32cbc2
Luis Tellez <luis@luistellez.com>
2022-07-18 12:06:45 -0700

    refactor: prepare enableValidation global function

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -4,13 +4,34 @@ class FormValidator {
     this._formElement = formElement;
   }
 
+  _showErrorMessage(input) {}
+
+  _hideErrorMessage(input) {}
+
   _checkInputValidity(input) {}
 
   _toggleSubmitButtonState(inputs) {}
 
   _setEventListeners() {}
 
-  enableValidation() {}
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    const inputs = [
+      this._formElement.querySelectorAll(this._formSelectors.inputSelector),
+    ];
+    const button = this._formElement.querySelector(
+      this._formSelectors.submitButtonSelector
+    );
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        this._checkInputValidity(input);
+        this._toggleSubmitButtonState(inputs);
+      }
+  }
 }
 
 export { FormValidator };
