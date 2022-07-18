c2943042cccd971decc34277a829db2e7522c204
Luis Tellez <luis@luistellez.com>
2022-07-17 22:49:23 -0700

    refactor: move code from index.js to utils.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,68 +2,6 @@ import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
 
 // Variable initialization
-const editButton = document.querySelector(".profile__edit-button");
-const modal = document.querySelector(".modal");
-const formName = document.querySelector(".modal__profile-name");
-const formTitle = document.querySelector(".modal__profile-title");
-const profileName = document.querySelector(".profile__name");
-const profileTitle = document.querySelector(".profile__title");
-const profileCloseButton = document.querySelector(".modal__close");
-const form = document.querySelector(".modal__form");
-const createPostButton = document.querySelector(".profile__post-button");
-const modalPost = document.querySelector(".modal-post");
-const postCloseButton = document.querySelector(".modal-post__close");
-
-//Controls for edit modal opening
-// Event listener for edit button click
-editButton.addEventListener("click", modalOpen);
-
-// Function to open modal and in the process set the values of the form fields same as the profile fields
-function modalOpen() {
-  modal.classList.add("modal_active");
-  formName.placeholder = profileName.textContent;
-  formTitle.placeholder = profileTitle.textContent;
-}
-
-//Controls for edit modal closing
-// Event listener for close button click
-profileCloseButton.addEventListener("click", modalClose);
-
-function modalClose() {
-  modal.classList.remove("modal_active");
-}
-
-// Event listener for form submit
-form.addEventListener("submit", updateNameAndTitle);
-
-//Function to update the profile name and title
-function updateNameAndTitle(evt) {
-  evt.preventDefault();
-
-  const formNameValue = formName.value;
-  const formTitleValue = formTitle.value;
-
-  profileName.textContent = formNameValue;
-  profileTitle.textContent = formTitleValue;
-
-  modalClose();
-}
-
-// Event listener for createPostButton button click
-createPostButton.addEventListener("click", modalPostOpen);
-
-// Function to open modal and in the process set the values of the form fields same as the profile fields
-function modalPostOpen() {
-  modalPost.classList.add("modal-post_active");
-}
-
-//Controls for edit modal closing
-// Event listener for close button click
-postCloseButton.addEventListener("click", modalPostClose);
-
-function modalPostClose() {
-  modalPost.classList.remove("modal-post_active");
-}
 
 // Code to use cards with the template and array
 // Select where to put the template cards
@@ -165,33 +103,4 @@ images.forEach((image) => {
       lightbox.classList.remove("active");
     }
   });
-});
-
-// Close modal on esc key press
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    modalPostClose();
-    modalClose();
-  }
-});
-
-// Close modal on click outside of the lightbox
-lightbox.addEventListener("click", (e) => {
-  if (e.target === lightbox) {
-    lightbox.classList.remove("active");
-  }
-});
-
-// Close modal on click outside of the profile modal
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modalClose();
-  }
-});
-
-// Close modal on click outside of the new post modal
-modalPost.addEventListener("click", (e) => {
-  if (e.target === modalPost) {
-    modalPostClose();
-  }
 });
