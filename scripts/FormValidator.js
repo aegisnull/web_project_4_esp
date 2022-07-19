19d0da309d2a7b3033f259d5b57a7a667048a892
Luis Tellez <luis@luistellez.com>
2022-07-18 22:57:36 -0700

    refactor: add showErrorMessage function

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,10 +1,23 @@
 class FormValidator {
   constructor(formSelectors, formElement) {
     this._formSelectors = formSelectors;
-    this._formElement = formElement;
+    this._form = formElement;
+    this._formSelector = formSelectors.formSelector;
+    this._inputSelector = formSelectors.inputSelector;
+    this._submitButtonSelector = formSelectors.submitButtonSelector;
+    this._inactiveButtonClass = formSelectors.inactiveButtonClass;
+    this._inputErrorClass = formSelectors.inputErrorClass;
+    this._errorClass = formSelectors.errorClass;
+    this._inputList = [...this._form.querySelectorAll(this._inputSelector)];
+    this._buttonElement = this._form.querySelector(this._submitButtonSelector);
   }
 
-  _showErrorMessage() {}
+  _showErrorMessage(inputElement, errorMessage) {
+    const errorElement = this._form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
 
   _hideErrorMessage() {}
 
@@ -18,3 +31,21 @@ class FormValidator {
 }
 
 export { FormValidator };
+
+/* --------------------------------- Verification --------------------------------- */
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__form-input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input-type-error",
+  errorClass: "popup__error_visible",
+};
+
+const profileFormValidator = new FormValidator(
+  validationConfig,
+  submitProfileEdit
+);
+const placesFormValidator = new FormValidator(validationConfig, submitNewPlace);
+placesFormValidator.enableValidation();
+profileFormValidator.enableValidation();
