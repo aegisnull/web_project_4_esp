1514d1601f929944106bee9d9910226a0fc453e5
Luis Tellez <luis@luistellez.com>
2022-07-22 13:34:35 -0700

    fix: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path"); // conecta la ruta a la configuración de webpack
 
 module.exports = {
+  devtool: "inline-source-map",
   entry: {
     main: "./src/pages/index.js",
   },
@@ -8,5 +9,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
     publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"), // especifica una carpeta desde donde servir la aplicación y su contenido
+    compress: true, // esto acelerará la carga de archivos en el modo de desarrollo
+    port: 8080, // abrirá tu página en localhost:8080 (puedes usar otro puerto)
+    open: true, // se abrirá automáticamente en el navegador después de ejecutar npm run dev
+    stats: "errors-only", //solo sale cuando se producen errores
   },
 };
