05e8c4682550b4193ccc6d46e10cf58abdb75b42
Luis Tellez <luis@luistellez.com>
2022-07-26 12:31:06 -0700

    feature: add open and close public methods on Popup class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,4 +2,12 @@ class Popup {
   constructor(popup) {
     this._popup = popup;
   }
+
+  open() {
+    this._popup.classList.add("modal_active");
+  }
+
+  close() {
+    this._popup.classList.remove("modal_active");
+  }
 }
