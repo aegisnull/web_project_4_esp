5b606e4372e8444436465a393418007d2eb6f317
Luis Tellez <luis@luistellez.com>
2022-08-14 21:44:51 -0700

    feat: declare API const in index.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,5 +3,7 @@ class Api {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
+
+  //the link for the petition via GET is https://around.nomoreparties.co/v1/groupId/users/me
 }
 export { Api };
