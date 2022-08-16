e2f998629e3b3ba976b1c25ea8146f22a603d729
Luis Tellez <luis@luistellez.com>
2022-08-16 16:10:51 -0700

    feat: add getUserInfo method

--- src/components/Api.js
+++ src/components/Api.js
@@ -5,8 +5,21 @@ class Api {
   }
 
   //the link for the petition via GET is https://around.nomoreparties.co/v1/groupId/users/me
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+        console.log(res);
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
 
   // Code to declare API
+}
+
 const api = new Api({
   baseUrl: "https://around.nomoreparties.co/v1/cohort-1-es",
   headers: {
@@ -14,5 +27,5 @@ const api = new Api({
     "Content-Type": "application/json",
   },
 });
-}
+
 export { Api };
