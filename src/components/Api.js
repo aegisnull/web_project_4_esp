a034840a82ccd11b8c645b3226fe4cf19b2a3df8
Luis Tellez <luis@luistellez.com>
2022-09-09 16:33:58 -0700

    feat: create getUserInfo API method

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,91 +3,26 @@ class Api {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
-  _checkResponse(res) {
-    return res.ok ? res.json() : Promise.reject("Error!" + res.statusText);
-  }
-
-  getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
-  }
-  _checkResponse(res) {
-    return res.ok ? res.json() : Promise.reject("Error!" + res.statusText);
-  }
 
   //GET https://around.nomoreparties.co/v1/groupId/users/me
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
-  }
-  getAllInfo() {
-    return Promise.all([this.getUserInfo(), this.getInitialCards()]);
-  }
+    // Variables
+    const username = document.querySelector(".profile__name");
+    const userPosition = document.querySelector(".profile__title");
 
-  //PATCH https://around.nomoreparties.co/v1/groupId/users/me
-  setUserInfo({ name, about }) {
+    // FETCH request
     return fetch(`${this._baseUrl}/users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        name,
-        about,
-      }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
-  }
-  //POST https://around.nomoreparties.co/v1/groupId/cards
-  addCard({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({
-        name,
-        link,
-      }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
-  }
-  removeCard(cardId) {
-    //fetch cards + cardid
-    return fetch(this._baseUrl + "/cards/" + cardId, {
-      method: "DELETE",
+      method: "GET",
       headers: this._headers,
     })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
-  }
-  //PATCH https://around.nomoreparties.co/v1/groupId/users/me/avatar
-  setAvatar({ avatar }) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar,
-      }),
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log("Error! " + err));
-  }
-  // PUT https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
-  // DELETE https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
-  changeLikeCardStatus(cardId, like) {
-    //PUT AND DELETE
-    const whichMethod = like ? "DELETE" : "PUT";
-    return fetch(this._baseUrl + "/cards/likes/" + cardId, {
-      method: whichMethod,
-      headers: this._headers,
-    })
-      .then(this._checkResponse)
-      .catch((err) => console.log(err));
+      .then((res) => res.json())
+      .then((data) => {
+        username.textContent = data.name;
+        userPosition.textContent = data.about;
+      })
+      .catch((err) => {
+        console.log("Error. La solicitud ha fallado");
+      });
   }
 }
 export { Api };
